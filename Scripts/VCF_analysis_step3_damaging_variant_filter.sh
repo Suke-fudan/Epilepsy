eb1cb06d55d1b26e9d049e4329c5a67f9f5735c2
Suke-fudan <84702788+Suke-fudan@users.noreply.github.com>
2026-02-28 14:45:02 +0800

    Update VCF_analysis_step3_damaging_variant_filter.sh

--- Scripts/VCF_analysis_step3_damaging_variant_filter.sh
+++ Scripts/VCF_analysis_step3_damaging_variant_filter.sh
@@ -1,1 +1,2 @@
-cat candidate_gene_HB_CMaf_Het.vcf  |perl -lane 'if(/AF_eas=(.*)?;AF_fin.*?1000g2015aug_eas=(.*)?;CLNALLELEID/){if(($1 <= 0.001||$1 eq "\.")&&($2 <= 0.001||$2 eq "\.")){print $_}}'  >AF.vcf
+cat GATK_Exon_filter_split_head_stat.vcf |perl -lane 'if(/AF_eas=(.*)?;AF_fin.*?1000g2015aug_eas=(.*)?;CLNALLELEID/){if(($1 <= 0.001||$1 eq "\.")&&($2 <= 0.001||$2 eq "\.")){print $_}}'  >AF.vcf
+cat AF.vcf | 
