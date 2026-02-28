965c6a1b125804b0ae967ffd7da705ca75b533ea
Suke-fudan <84702788+Suke-fudan@users.noreply.github.com>
2026-02-28 10:42:38 +0800

    Create VCF_analysis_step2_variant_filter.sh

---
+++ Scripts/VCF_analysis_step2_variant_filter.sh
@@ -0,0 +1,3 @@
+cat epilepsy_cohort.hg19_multianno.vcf |perl -lane 'if(/Func.refGene=exonic/){print $_}if(/Func.refGene=ncRNA_exonic/){print $_}if(/Func.refGene=splicing/){print $_}' |perl -lane 'if(/ExonicFunc.refGene=frameshift/){print $_}if(/ExonicFunc.refGene=nonframeshift/){print $_}if(/ExonicFunc.refGene=start/){print $_}if(/ExonicFunc.refGene=stop/){print $_}if(/ExonicFunc.refGene=nonsynonymous/){print $_}elsif(/Func.refGene=splicing/){print $_}' |sed 's/ /_/g' > GATK_Exon_filter.vcf
+awk -f /public/group_share_data/fan_lab/suke/Epilepsy/VQSR/annovar/split_new.awk GATK_Exon_filter.vcf >GATK_Exon_filter_split.vcf     ## split_new.awk is provided as a separate file
+cat epilepsy_cohort.vcf |awk '{if($0 ~ /^#CH/){print}}' >epilepsy_cohort_head.txt
