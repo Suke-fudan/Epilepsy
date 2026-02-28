3e7412c1add583803aacde077853db79c123a592
Suke-fudan <84702788+Suke-fudan@users.noreply.github.com>
2026-02-28 09:32:05 +0800

    Update WES_pipeline_step2_trimadap.sh

--- Scripts/WES_pipeline_step2_trimadap.sh
+++ Scripts/WES_pipeline_step2_trimadap.sh
@@ -1,3 +1,3 @@
 threads=2
-trimadap-mt -p ${threads} ${sample}_combined_R1.fastq.gz  | gzip  >/WES/trimadap_fastq/${sample}_combined_R1_trimadap.fastq.gz
-trimadap-mt -p ${threads} ${sample}_combined_R2.fastq.gz | gzip  >/WES/trimadap_fastq/${sample}_combined_R2_trimadap.fastq.gz
+trimadap-mt -p ${threads} ${sample}_combined_R1.fastq.gz  | gzip  >/public/home/fan_lab/suke/Epilepsy/WES/trimadap_fastq/${sample}_combined_R1_trimadap.fastq.gz
+trimadap-mt -p ${threads} ${sample}_combined_R2.fastq.gz | gzip  >/public/home/fan_lab/suke/Epilepsy/WES/trimadap_fastq/${sample}_combined_R2_trimadap.fastq.gz
