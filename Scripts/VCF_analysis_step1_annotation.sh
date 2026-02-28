a1a3cb3e2f29490bca09e03952c97bb59374eeeb
Suke-fudan <84702788+Suke-fudan@users.noreply.github.com>
2026-02-28 14:19:22 +0800

    Update VCF_analysis_step1_annotation.sh

--- Scripts/VCF_analysis_step1_annotation.sh
+++ Scripts/VCF_analysis_step1_annotation.sh
@@ -4,4 +4,4 @@ vcf=epilepsy_cohort.vcf
 threads=8
 out_name=epilepsy_cohort
 
-/public/home/fan_lab/suke/softwares/annovar_20210909/annovar/table_annovar.pl ${vcf} /public/home/fan_lab/suke/softwares/annovar_20210909/annovar/humandb -thread ${threads} -buildver hg19 -out ${out_name} -remove -protocol refGene,cytoBand,avsnp150,avsift,dbnsfp42a,mcap13,revel,dbscsnv11,exac03,exac03nonpsych,gene4denovo201907,gnomad211_exome,1000g2015aug_all,1000g2015aug_eas,clinvar_20230226,intervar_20180118 -operation g,r,f,f,f,f,f,f,f,f,f,f,f,f,f,f -nastring . -vcfinput
+/public/home/fan_lab/suke/softwares/annovar/table_annovar.pl ${vcf} /public/home/fan_lab/suke/softwares/annovar/humandb -thread ${threads} -buildver hg19 -out ${out_name} -remove -protocol refGene,cytoBand,avsnp150,avsift,dbnsfp42a,mcap13,revel,dbscsnv11,exac03,exac03nonpsych,gene4denovo201907,gnomad211_exome,1000g2015aug_all,1000g2015aug_eas,clinvar_20230226,intervar_20180118 -operation g,r,f,f,f,f,f,f,f,f,f,f,f,f,f,f -nastring . -vcfinput
