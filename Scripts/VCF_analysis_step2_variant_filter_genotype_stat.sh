7b0af7ae4b2010c3a5c40f18dd13665f86ca3587
Suke-fudan <84702788+Suke-fudan@users.noreply.github.com>
2026-02-28 14:31:39 +0800

    Update VCF_analysis_step2_variant_filter_genotype_stat.sh

--- Scripts/VCF_analysis_step2_variant_filter_genotype_stat.sh
+++ Scripts/VCF_analysis_step2_variant_filter_genotype_stat.sh
@@ -4,5 +4,3 @@ cat epilepsy_cohort.vcf |awk '{if($0 ~ /^#CH/){print}}' >epilepsy_cohort_head.tx
 cat epilepsy_cohort_head.txt GATK_Exon_filter_split.vcf >GATK_Exon_filter_split_head.vcf
 python genotype_stat_GATK.py GATK_Exon_filter_split_head.vcf > GATK_Exon_filter_split_head_stat.vcf  ## genotype_stat_GATK.py is provided as a separate file
 cat GATK_Exon_filter_split.vcf |awk -F '\t' '{for(a=10;a<=NF;a++){split($a,b,":");print b[1],"\n"}}' |sort |uniq -c >genotypes.stat
-
-
