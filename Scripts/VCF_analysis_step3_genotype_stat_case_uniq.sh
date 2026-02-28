cc954400d6757784dd7e30c62a4b8ee9564eeb2c
Suke-fudan <84702788+Suke-fudan@users.noreply.github.com>
2026-02-28 16:07:11 +0800

    Update VCF_analysis_step3_genotype_stat_case_uniq.sh

--- Scripts/VCF_analysis_step3_genotype_stat_case_uniq.sh
+++ Scripts/VCF_analysis_step3_genotype_stat_case_uniq.sh
@@ -1,4 +1,2 @@
-
-
 python genotype_stat_GATK.py GATK_Exon_filter_split_head.vcf > GATK_Exon_filter_split_head_stat.vcf  ## genotype_stat_GATK.py is provided as a separate file
 cat GATK_Exon_filter_split.vcf |awk -F '\t' '{for(a=10;a<=NF;a++){split($a,b,":");print b[1],"\n"}}' |sort |uniq -c >genotypes.stat
